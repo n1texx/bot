b69831448c71b26c03a824de704cf6553c9a5ea1
n1texx <145290649+n1texx@users.noreply.github.com>
2023-09-18 19:18:26 +0300

    Delete keyboard.py

--- keyboard.py
+++
@@ -1,9 +0,0 @@
-from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, ReplyKeyboardRemove, InlineKeyboardButton, InlineKeyboardMarkup
-
-mainmenu_Chat = KeyboardButton('⚡️ЗАРЯД⚡️')
-mainmenu_Dengi = KeyboardButton('Чат💬')
-mainmenu_Zaryad = KeyboardButton('Выплаты💸')
-mainmenu_Manual = KeyboardButton('📚Мануалы')
-mainmenu_Project = KeyboardButton('💼О проекте')
-
-mainmenu = ReplyKeyboardMarkup(resize_keyboard = True).add(mainmenu_Dengi, mainmenu_Chat, mainmenu_Zaryad, mainmenu_Manual, mainmenu_Project)
