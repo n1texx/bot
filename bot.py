ae076a72fd01c9057a70707e58633d4fce147ee5
n1texx <145290649+n1texx@users.noreply.github.com>
2023-09-18 19:17:00 +0300

    Delete bot.py

--- bot.py
+++
@@ -1,8 +0,0 @@
-from aiogram import Bot, Dispatcher
-from aiogram.contrib.fsm_storage.memory import MemoryStorage
-
-from configure import cfg
-
-storage = MemoryStorage()
-bot = Bot(token=cfg['token'])
-dp = Dispatcher(bot, storage=storage)
